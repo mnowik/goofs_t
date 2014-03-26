91c928d2855a3816b3a365768662b034ca49c655
ffff <ffff@berkeley.edu>
2014-03-26 00:53:05 -0700

    essentially done for experimental purposes

--- app/views.py
+++ app/views.py
@@ -9,21 +9,27 @@ REDIRECT_URI = 'http://127.0.0.1:5000/authorize_callback'
 r = praw.Reddit('goof')
 r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
+
+# index controls (1) login (2) post display
 @app.route('/')
 @app.route('/index')
 def index():
 
 	# if user is already authenticated
 	if 'reddit_oauth' in session:
+		if session['reddit_oauth'] is not None:
 		
-		# display posts...
-		posts = r.get_front_page()
-		return render_template("index.html",posts=posts)
+			# display posts...
+			print('getting fronptage')
+			posts = r.get_front_page(limit=100)
+			return render_template("index.html",posts=posts)
 
 	# otherwise, start authentication process
-	link_no_refresh = r.get_authorize_url('UniqueKey', 'read')
-	return render_template("index.html", login_link=link_no_refresh)
+	link_with_refresh = r.get_authorize_url('UniqueKey', 'read', True)
+	return render_template("index.html", login_link=link_with_refresh)
 
+
+# this is called when reddit responds to our auth request
 @app.route('/authorize_callback')
 def authorized():
 
@@ -39,3 +45,12 @@ def authorized():
 	# redirect to index
 	return redirect(url_for('index'))
 
+
+# logout routine
+@app.route('/logout')
+def logout():
+
+	# clear the user's oath token from session
+	session['reddit_oauth'] = None
+
+	return redirect(url_for('index'))

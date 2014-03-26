f0b521b00b4f010ab794a7b1e44371b4facf43c9
ffff <ffff@berkeley.edu>
2014-03-25 22:41:02 -0700

    preliminarily working - needs to load more + styling + opening in tabs

--- app/views.py
+++ app/views.py
@@ -1,11 +1,41 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
 import praw
 from app import app
+
+CLIENT_ID = 'QOkJLc1GlplZgA'
+CLIENT_SECRET = 'S5WFGh7QoKFKFxVtsN8YYzThyb4'
+REDIRECT_URI = 'http://127.0.0.1:5000/authorize_callback'
+
+r = praw.Reddit('goof')
+r.set_oauth_app_info(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	r = praw.Reddit(user_agent='my_cool_application')
-	submissions = r.get_subreddit('opensource').get_hot(limit=5)
-	return render_template("index.html",
-    	submissions=submissions)
+
+	# if user is already authenticated
+	if 'reddit_oauth' in session:
+		
+		# display posts...
+		posts = r.get_front_page()
+		return render_template("index.html",posts=posts)
+
+	# otherwise, start authentication process
+	link_no_refresh = r.get_authorize_url('UniqueKey', 'read')
+	return render_template("index.html", login_link=link_no_refresh)
+
+@app.route('/authorize_callback')
+def authorized():
+
+	# get the auth code from the request reddit sent us
+	code = request.args.get('code', '')
+
+	# store this code in our session 
+	session['reddit_oauth'] = code
+
+	# apply this code to praw
+	info = r.get_access_information(code)
+	
+	# redirect to index
+	return redirect(url_for('index'))
+
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 #!bin/python
 from app import app
+import praw
+
+SECRET_KEY = 'a555c33332'
+app.secret_key = SECRET_KEY
 app.run(debug = True)
